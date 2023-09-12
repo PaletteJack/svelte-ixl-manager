f3d385d855bd338aa4bcf4e1644c91637d58f47e
PaletteJack <barbozalouie@gmail.com>
2023-09-12 14:44:08 -0700

    aslkdfj

--- src/routes/admin/+page.server.js
+++ src/routes/admin/+page.server.js
@@ -1,4 +1,5 @@
 import Papa from 'papaparse'
+import { insertEnrollments, insertGrade, insertSchool, insertSection, insertSectionTeachers, insertStudent, insertTeacher } from '../../lib/loadData';
 
 
 /** @type {import('./$types').Actions} */
@@ -6,16 +7,47 @@ export const actions = {
     submitFiles: async ({ request, locals: { db }}) => {
         const formData = await request.formData()
         const files = formData.getAll('csvs');
-        
-        for (const file of files) {
-            console.log(file);
-            // const content = await file.text();
-            // const parsedData = Papa.parse(content, {
-            //     header: true,
-            //     dynamicTyping: true
-            // })
+        let insertFunction;
 
-            // console.log(parsedData);
+        for (const file of files) {
+
+            switch(item) {
+                case'Schools.csv':
+                    insertFunction = insertSchool;
+                    break;
+                case'Grades.csv':
+                    insertFunction = insertGrade;
+                    break;
+                case'Teachers.csv':
+                    insertFunction = insertTeacher;
+                    break;
+                case'Students.csv':
+                    insertFunction = insertStudent;
+                    break;
+                case'Sections.csv':
+                    insertFunction = insertSection;
+                    break;
+                case'Section-Assignments.csv':
+                    insertFunction = insertSectionTeachers;
+                    break;
+                case'Enrollments.csv':
+                    insertFunction = insertEnrollments;
+                    break;
+                default:
+                    console.log("Could not find function associated with the file.");
+                    break;
+            }
+            const content = await file.text();
+            Papa.parse(content, {
+                header: true,
+                dynamicTyping: true,
+                skipEmptyLines: true,
+                complete: function(results) {
+                    results.data.forEach(row => {
+                        insertFunction(row, db);
+                    });
+                }
+            })
         }
     }
 };
